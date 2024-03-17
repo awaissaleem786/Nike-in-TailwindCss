2218bd85b34bc402e78ea0ccb2d60ac768fc101c
awais <se.awaissaleem@gmail.com>
2024-03-17 12:47:04 +0500

    Apply frammer-motion effect

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,20 +5,31 @@ import shoes from "../assets/shoe_image.png";
 import { Button } from "@mui/material";
 import filp from '../assets/flipkart.png'
 import amazon from '../assets/amazon.png';
-
+import{motion} from 'framer-motion'
 // const color = red[500];
 
 const Hero = () => {
   return (
     <>
-      <div className="main max-w-[1125px] pt-10 mx-auto flex items-center justify-center gap-14" styl={{height: "calc(100vh - 72px)"}}>
+      <div className="main row max-w-[1125px] pt-10 mx-auto flex items-center justify-center gap-14" styl={{height: "calc(100vh - 72px)"}}>
         <div className="max-w-[595px] flex flex-col gap-3 mx-auto">
-          <h1 className="font-poppins text-[100px] font-extrabold leading-[102px]">YOUR FEET DESERVE THE BEST</h1>
-          <p className="text-lg">
+          <div className="font-poppins text-[100px] font-extrabold leading-[102px]">
+            <motion.h1 initial={{opacity:0,x:-100}} 
+            animate={{opacity:1,x:0}} 
+            transition={{duration:1,delay:0.5}}
+            >YOUR FEET</motion.h1>
+            <motion.h1 initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{duration:1,delay:1}}> DESERVE</motion.h1>
+            <motion.h1 initial={{opacity:0,x:-100}} animate={{opacity:1,x:0}} transition={{duration:1,delay:1.5}}> THE BEST</motion.h1>
+            </div>
+          <motion.p
+          initial={{opacity:0,x:-100}}
+          animate={{opacity:1,x:0}}
+          transition={{duration:1,delay:2}}
+          className="text-lg">
             YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES.
-          </p>
+          </motion.p>
           <div className="button flex gap-8">
             <Button variant="contained" color="error">Shop Now</Button>
             <Button variant="outlined">Catagory</Button>
@@ -29,9 +40,9 @@ const Hero = () => {
            <img src={amazon} alt="" />
           </div>
         </div>
-        <div className="">
+        <motion.div initial={{opacity:0,y:-100}} animate={{opacity:1,y:0}} transition={{duration:1,delay:2.5}} >
           <img src={shoes} alt="" />
-        </div>
+        </motion.div>
       </div>
     </>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,50 @@
-import React from 'react'
-import { Button } from '@mui/material';
-import logo from '../assets/brand_logo.png'
+import React, { useState } from "react";
+import { Button } from "@mui/material";
+import logo from "../assets/brand_logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
+import { motion } from "framer-motion";
+const Navlist = () => {
+  return (
+    <ul className="sm:flex sm:gap-6">
+      <li href="#">Menu</li>
+      <li href="#">Location</li>
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+  );
+};
+
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
     <>
-      <nav className='container  mx-auto p-8 flex justify-between h-[70px]  items-center'>
+      <motion.nav
+      initial={{opacity:0,y:-100}}
+      animate={{opacity:1,y:0}}
+      transition={{duration:1}}
+      className="container  mx-auto p-8 flex justify-between h-[70px]  items-center">
         <div className="logo">
-            <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
-        <ul className='flex gap-6'>
-            <li href="#">Menu</li>
-            <li href="#">Location</li>
-            <li href="#">About</li>
-            <li href="#">Contact</li>
-        </ul>
-        <div>
-        <Button variant='contained'>Login</Button>
+        <div className="hidden sm:block">{<Navlist/>}</div>
+        <div className="flex">
+          <Button variant="contained">Login</Button>
+          <div className="p-2 sm:hidden">
+            {
+              show ? <CloseIcon sx={{fontSize:40}} onClick={()=>setShow(false)}/>: <MenuIcon sx={{fontSize:40}} onClick={()=>{setShow(!false)}} />
+
+            }
+          </div>
         </div>
-      </nav>
-      
+      </motion.nav>
+      {
+        show && <div>
+          <Navlist/>
+        </div>
+      }
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
